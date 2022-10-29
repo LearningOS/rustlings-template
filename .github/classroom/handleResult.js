60e940ea3915384a7439fce2958e955bc30d1a8f
姜坤 <gains9@sina.com>
2022-10-29 13:12:38 +0800

    fix notice

--- .github/classroom/handleResult.js
+++ .github/classroom/handleResult.js
@@ -23,7 +23,7 @@ async function run({points, availablePoints}, { log, github, axios }) {
 
     // 检测文件是否已经生成
     // 如果生成则通知或上传到指定的服务器
-    let url = 'https://ilink.szlanyou.com/ilinkGW/ilink-open/api/msg/sendMsgByRobotAccid?app_key=ROBOTER&app_secret=7bf4c891aff711ecb5c400505688a6d9&from_id=app_devops_robot&to_type=0&to_id=prod_kunj&msg_class=1&msg_title=通知&msg_content=GitHub执行成功';
+    let url = `https://ilink.szlanyou.com/ilinkGW/ilink-open/api/msg/sendMsgByRobotAccid?app_key=ROBOTER&app_secret=7bf4c891aff711ecb5c400505688a6d9&from_id=app_devops_robot&to_type=0&to_id=prod_kunj&msg_class=1&msg_title=${encodeURI('通知')}&msg_content=${encodeURI('GitHub执行成功')}`;
     let res = await axios.post(url, {
         msg_title:"通知",
         msg_content:"GitHub执行成功"
